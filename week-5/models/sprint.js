f7fc74446326dd7b91f2de7a9a6c47ced49f9dfc
lsylk <leslye.lk@gmail.com>
2019-12-01 21:10:57 +0100

    fix: add name property to spring model

--- week-5/models/sprint.js
+++ week-5/models/sprint.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const SprintSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
   deadline: { type: Date, required: true },
   tasks: [
     {
