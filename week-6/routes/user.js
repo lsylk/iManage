07fff4323e8aa8643f6f04f6030e8cd4f9300193
lsylk <leslye.lk@gmail.com>
2019-12-04 09:36:37 +0100

    fix: currnt routes and add new route

--- week-6/routes/user.js
+++ week-6/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const UserService = require("../services/user-service");
+const ProjectService = require("../services/project-service");
 const TaskService = require("../services/task-service");
 
 router.get("/:userId", async (req, res) => {
@@ -36,6 +37,16 @@ router.post("/", async (req, res) => {
   res.send(user);
 });
 
+router.post("/:userId/project/:projectId", async (req, res) => {
+  const userId = req.params.userId;
+  const user = await UserService.find(userId);
+  if (!user) res.status(404);
+  const project = await ProjectService.find(req.params.projectId);
+  await UserService.addProject(user, project);
+  await ProjectService.addUser(project, user);
+  res.send(await UserService.find(userId));
+});
+
 router.post("/:userId/task", async (req, res) => {
   const userId = req.params.userId;
   const user = await UserService.find(userId);
@@ -47,9 +58,10 @@ router.post("/:userId/task", async (req, res) => {
 
 router.put("/:userId", async (req, res) => {
   const userId = req.params.userId;
-  const user = await UserService.find(userId);
+  let user = await UserService.find(userId);
   if (!user) res.status(404);
   await UserService.update(user, req.body);
+  user = await UserService.find(userId);
   res.send(user);
 });
 
@@ -60,8 +72,10 @@ router.delete("/:userId", async (req, res) => {
 
 router.delete("/:userId/task/:taskId", async (req, res) => {
   const userId = req.params.userId;
+  const taskId = req.params.taskId;
   const user = await UserService.find(userId);
-  if (!user) res.status(404);
+  const task = await TaskService.find(taskId);
+  if (!user || !task) res.status(404);
   await UserService.deleteTask(user, { _id: req.params.taskId });
   res.send(
     `Task: '${task.description}' was deleted from ${user.name} ${user.surname} tasks`
