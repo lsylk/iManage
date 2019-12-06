b4b03bc38b2ba9e85968f1258ae07cefb294cfff
lsylk <leslye.lk@gmail.com>
2019-12-06 19:09:01 +0100

    fix: styling

--- week-7/frontend/src/components/TaskCard.vue
+++ week-7/frontend/src/components/TaskCard.vue
@@ -1,37 +1,30 @@
 <template>
-  <div>
-    <md-card :md-with-hover="true">
-      <md-card-area>
-        <md-card-header>
-          <div class="md-subhead">
-            <span
-              ><b>Status: {{ task.status }}</b></span
-            >
-            <md-badge class="md-square" :md-content="task.type" />
-          </div>
-        </md-card-header>
-
-        <md-card-content>
-          {{ task.description }}
-        </md-card-content>
-      </md-card-area>
-
-      <md-card-actions md-alignment="right" v-if="task.users && task.users.length > 0">
-        <div class="users">
-          <span v-for="user in task.users" :key="user._id">
-            <md-avatar class="md-avatar-icon">{{ formatUserName(user) }}</md-avatar>
-          </span>
-          <md-avatar class="md-avatar-icon md-small">JD</md-avatar>
-          <md-avatar class="md-avatar-icon md-small">JP</md-avatar>
-          <md-avatar class="md-avatar-icon md-small">SM</md-avatar>
-          <md-avatar class="md-avatar-icon md-small">LK</md-avatar>
+  <md-card :md-with-hover="true">
+    <md-card-area>
+      <md-card-header>
+        <div class="md-subhead">
+          <span
+            ><b>Status: {{ task.status }}</b></span
+          >
+          <md-badge class="md-square" :md-content="task.type" />
         </div>
-        <md-button class="md-icon-button md-list-action">
-          <md-icon class="md-primary">edit</md-icon>
-        </md-button>
-      </md-card-actions>
-    </md-card>
-  </div>
+      </md-card-header>
+
+      <md-card-content>
+        {{ task.description }}
+      </md-card-content>
+    </md-card-area>
+    <md-card-actions md-alignment="right" v-if="task.users && task.users.length > 0">
+      <div class="users">
+        <span v-for="user in task.users" :key="user._id">
+          <md-avatar class="md-avatar-icon md-small">{{ formatUserName(user) }}</md-avatar>
+        </span>
+      </div>
+      <md-button class="md-icon-button md-list-action">
+        <md-icon class="md-primary">edit</md-icon>
+      </md-button>
+    </md-card-actions>
+  </md-card>
 </template>
 
 <script>
@@ -41,12 +34,12 @@ export default {
     task: {
       type: Object,
       default: () => ({
-        description: 'description',
-        type: 'type',
+        description: '',
+        type: '',
         notes: [],
         comments: [],
-        users: [{ name: 'John', surname: 'Doe' }],
-        status: 'status',
+        users: [{ name: '', surname: '' }],
+        status: '',
       }),
     },
   },
@@ -62,19 +55,29 @@ export default {
 
 <style lang="scss" scoped>
 .md-card {
-  width: 320px;
+  width: 300px;
   margin: 4px;
   display: inline-block;
   vertical-align: top;
-  .md-subhead {
-    display: flex;
-    justify-content: space-between;
-    align-content: center;
-    opacity: unset;
-
-    .md-badge {
-      position: relative;
+  .md-card-header {
+    padding: 6px 16px;
+    .med-title {
+      font-size: 20px;
     }
+    .md-subhead {
+      display: flex;
+      justify-content: space-between;
+      align-content: center;
+      opacity: unset;
+
+      .md-badge {
+        position: relative;
+      }
+    }
+  }
+  .md-card-content {
+    padding: 0;
+    margin: 0px 10px 10px 10px;
   }
 }
 .users {
