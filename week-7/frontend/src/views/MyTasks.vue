09f61356bf18fd9bb1a5bcf41b983b3e790481bb
lsylk <leslye.lk@gmail.com>
2019-12-08 00:43:43 +0100

    feature: add my tasks view

---
+++ week-7/frontend/src/views/MyTasks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    My Tasks
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  name: 'MyTasks',
+  components: {},
+  computed: {
+    ...mapState(['user']),
+  },
+  methods: {
+    ...mapActions(['fetchUser']),
+  },
+  created() {
+    this.fetchUser(this.$route.params.id);
+  },
+};
+</script>
