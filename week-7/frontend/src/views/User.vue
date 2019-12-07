af79e8c5bd7cd304d01e48fb15854b0ca95aac7b
lsylk <leslye.lk@gmail.com>
2019-12-08 00:41:37 +0100

    chore: clean up

--- week-7/frontend/src/views/User.vue
+++ week-7/frontend/src/views/User.vue
@@ -1,21 +1,16 @@
 <template>
   <div class="user">
-      USERS
-    <span v-for="project in projects" :key="project.id">
-      <ItemCard :item="project" />
-    </span>
+      Profile
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
 
-import ItemCard from '@/components/ItemCard.vue';
 
 export default {
   name: 'User',
   components: {
-    ItemCard,
   },
   computed: {
     ...mapState(['user']),
