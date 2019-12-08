eb8ad3109febfdaf6214e80366dd15ed4c43df7e
lsylk <leslye.lk@gmail.com>
2019-12-08 17:59:53 +0100

    fix: add content and style

--- week-7/frontend/src/views/MyTasks.vue
+++ week-7/frontend/src/views/MyTasks.vue
@@ -1,23 +1,58 @@
 <template>
-  <div>
-    My Tasks
+  <div class="myTasks">
+    <div id="myTasksLeftContent">
+      <div v-for="task in user.tasks" :key="task._id">
+        <TaskCard :task="task" />
+      </div>
+    </div>
+    <div id="myTasksRightContent"><TaskModal /></div>
   </div>
 </template>
 
 <script>
+import TaskCard from '@/components/TaskCard.vue';
+import TaskModal from '@/components/modals/TaskModal.vue';
 import { mapState, mapActions } from 'vuex';
 
 export default {
   name: 'MyTasks',
-  components: {},
+  components: {
+    TaskCard,
+    TaskModal,
+  },
   computed: {
-    ...mapState(['user']),
+    ...mapState({
+      user: state => state.user.item,
+    }),
   },
   methods: {
-    ...mapActions(['fetchUser']),
+    ...mapActions({
+      fetchUser: 'user/fetchUser',
+    }),
   },
   created() {
     this.fetchUser(this.$route.params.id);
   },
 };
 </script>
+
+<style>
+.myTasks {
+  display: grid;
+  grid-template-areas: 'myTasksLeftContent myTasksRightContent';
+  grid-template-columns: 95% 5%;
+  margin: 16px;
+}
+
+#myTasksLeftContent {
+  grid-area: myTasksLeftContent;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-wrap: wrap;
+}
+
+#myTasksRightContent {
+  grid-area: myTasksRightContent;
+}
+</style>
