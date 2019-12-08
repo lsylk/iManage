f8feca7118a4a3f34bd6982e3352ec7459561b82
lsylk <leslye.lk@gmail.com>
2019-12-08 14:26:04 +0100

    fix: my projects views style and content

--- week-7/frontend/src/views/MyProjects.vue
+++ week-7/frontend/src/views/MyProjects.vue
@@ -1,49 +1,74 @@
 <template>
-  <div class="projects">
-    <span
-      v-for="project in projects"
-      :key="project._id"
-      @click="
-        goToProject(project._id);
-        fetchProject(project._id);
-      "
-    >
-      <ProjectCard :item="project" />
-    </span>
+  <div class="myProjects">
+    <div id="myProjectsLeftContent">
+      <div
+        v-for="project in user.projects"
+        :key="project._id"
+        @click="
+          goToProject(project);
+          fetchProject(project._id);
+        "
+      >
+        <ProjectCard :item="project" />
+      </div>
+    </div>
+
+    <div id="myProjectsRightContent"><ProjectModal /></div>
   </div>
 </template>
 
 <script>
 import ProjectCard from '@/components/ProjectCard.vue';
+import ProjectModal from '@/components/modals/ProjectModal.vue';
 import { mapState, mapActions } from 'vuex';
 
 export default {
   name: 'Project',
   components: {
     ProjectCard,
+    ProjectModal,
   },
   computed: {
     ...mapState({
+      pageHeader: state => state.pageHeader,
+
       user: state => state.user.item,
     }),
   },
   methods: {
-    ...mapActions({ fetchUser: 'project/fetchUser', fetchProject: 'project/fetchProject' }),
-    goToProject(projectId) {
-      this.$router.push({ name: 'project', params: { id: projectId } });
+    ...mapActions({
+      updatePageHeader: 'updatePageHeader',
+      fetchUser: 'user/fetchUser',
+      fetchProject: 'project/fetchProject',
+    }),
+    goToProject(project) {
+      this.updatePageHeader(`Project: ${project.name}`);
+
+      this.$router.push({ name: 'project', params: { id: project._id } });
     },
   },
   created() {
-    this.fetchUser();
+    this.fetchUser('5debedd01c51afd731f72496');
   },
 };
 </script>
 
 <style>
-.projects {
+.myProjects {
+  display: grid;
+  grid-template-areas: 'myProjectsLeftContent myProjectsRightContent';
+  grid-template-columns: 95% 5%;
+}
+
+#myProjectsLeftContent {
+  grid-area: myProjectsLeftContent;
   display: flex;
   justify-content: center;
   align-content: center;
   flex-wrap: wrap;
+}
+
+#myProjectsRightContent {
+  grid-area: myProjectsRightContent;
 }
 </style>
