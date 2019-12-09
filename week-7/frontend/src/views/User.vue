6be1f7512ce859a4f018c8685046f1fe79dd3da9
lyslk <leslye.lk@gmail.com>
2019-12-09 18:21:25 +0100

    fix: add aria-labels and style user information box

--- week-7/frontend/src/views/User.vue
+++ week-7/frontend/src/views/User.vue
@@ -4,12 +4,12 @@
       <img :src="`https://picsum.photos/2000/250?random=007`" alt="Cover Image" />
     </div>
 
-    <div class="profileContent">
+    <div class="profileContent md-elevation-10">
       <img :src="`https://picsum.photos/200?random=${this.$route.params.id}`" alt="Random profile picture" />
-      <div class="md-title">{{ formatUserName() }}</div>
-      <div class="md-subhead">{{ user.position }}</div>
-      <div class="md-subhead">Projects: {{ user.projects.length }}</div>
-      <div class="md-subhead">Tasks: {{ user.tasks.length }}</div>
+      <div class="md-title" aria-label="User's full name">{{ formatUserName() }}</div>
+      <div class="md-subhead" aria-label="User's position">{{ user.position }}</div>
+      <div class="md-subhead" aria-label="The number of projects the user has">Projects: {{ user.projects.length }}</div>
+      <div class="md-subhead" aria-label="The number of tasks the user has">Tasks: {{ user.tasks.length }}</div>
     </div>
   </div>
 </template>
@@ -56,6 +56,9 @@ export default {
 
 .profileContent {
   position: relative;
+  margin: 0px 50px 100px 50px;
+  padding-bottom: 20px;
+  border-top: none;
 }
 .profileContent img {
   border-radius: 50%;
